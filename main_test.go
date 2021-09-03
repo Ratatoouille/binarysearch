f2b45bef18c4c81a32e2882262fdbbd800236be9
Aleksey <54743508+Ratatoouille@users.noreply.github.com>
2021-09-03 21:27:40 +0700

    refactoring tests

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -16,42 +15,103 @@ var testSlice = []string{
 	"robert",
 }
 
-var notSortedSlice = []string{
-	"bob",
-	"alice",
-	"john",
+var sortSliceTests = []struct {
+	in  []string
+	out []string
+}{
+	{
+		[]string{
+			"bob",
+			"alice",
+			"john",
+		},
+		[]string{
+			"alice",
+			"bob",
+			"john",
+		},
+	},
+	{
+		[]string{
+			"bob",
+			"alice",
+			"john",
+			"bob",
+			"alice",
+			"john",
+		},
+		[]string{
+			"alice",
+			"alice",
+			"bob",
+			"bob",
+			"john",
+			"john",
+		},
+	},
 }
 
-var sortedSlice = []string{
-	"alice",
-	"bob",
-	"john",
+var searchTests = []struct {
+	slice       []string
+	target      string
+	targetIndex int
+}{
+	{
+		[]string{
+			"alice",
+			"bob",
+			"john",
+		},
+		"alice",
+		0,
+	},
+	{
+		[]string{
+			"alice",
+			"bob",
+			"john",
+		},
+		"john",
+		2,
+	},
+	{
+		[]string{
+			"alice",
+			"bob",
+			"john",
+		},
+		"kate",
+		-1,
+	},
 }
 
-func TestOkIsSorted(t *testing.T) {
-	if isSorted(sortedSlice) {
-		fmt.Println("Test isSort OK: slice is sorted")
-		return
+func TestIsSorted(t *testing.T) {
+	for i, tt := range sortSliceTests {
+		t.Run("isSorted"+string(rune(i)), func(t *testing.T) {
+			if !isSorted(tt.in) {
+			} else {
+				t.Errorf("Test isSorted failed")
+			}
+			if isSorted(tt.out) {
+			} else {
+				t.Errorf("Test isSorted failed")
+			}
+		})
 	}
-	t.Errorf("Test isSort failed")
-}
-
-func TestFailIsSorted(t *testing.T) {
-	if !isSorted(notSortedSlice) {
-		fmt.Println("Test isSort OK: slice not sorted")
-		return
-	}
-	t.Errorf("Test isSort failed")
 }
 
 func TestSortSlice(t *testing.T) {
-	sortSlice(notSortedSlice)
-	if !equal(notSortedSlice, sortedSlice) {
-		t.Errorf("Test sortSlice failed")
+	for i, tt := range sortSliceTests {
+		t.Run("sort"+string(rune(i)), func(t *testing.T) {
+			sortSlice(tt.in)
+			if !equal(tt.in, tt.out) {
+				t.Errorf("Test sortSlice failed")
+			}
+		})
 	}
 }
 
-func TestOkReadFile(t *testing.T) {
+func TestReadFile(t *testing.T) {
 	slice, err := readFile("./test_data.txt")
 	if err != nil {
 		t.Errorf("Test readFile failed: %s", err)
@@ -63,24 +123,14 @@ func TestOkReadFile(t *testing.T) {
 	}
 }
 
-func TestFindHightBinarySearchString(t *testing.T) {
-	i := binarySearchString(sortedSlice, "alice")
-	if i != 0 {
-		t.Errorf("Test binarySearchString failed: target wasn't found")
-	}
-}
-
-func TestFindLowBinarySearchString(t *testing.T) {
-	i := binarySearchString(sortedSlice, "john")
-	if i != 2 {
-		t.Errorf("Test binarySearchString failed: target wasn't found")
-	}
-}
-
-func TestNotFindBinarySearchString(t *testing.T) {
-	i := binarySearchString(sortedSlice, "kate")
-	if i != -1 {
-		t.Errorf("Test binarySearchString failed: target was found")
+func TestBinarySearchString(t *testing.T) {
+	for i, tt := range searchTests {
+		t.Run("sort"+string(rune(i)), func(t *testing.T) {
+			index := binarySearchString(tt.slice, tt.target)
+			if index != tt.targetIndex {
+				t.Errorf("Test BinarySearchString failed")
+			}
+		})
 	}
 }
 
